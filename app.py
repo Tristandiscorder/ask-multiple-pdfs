d7f0861532960944baeeb378e646896f92363740
Tristandiscorder <ohsama9114@gmail.com>
2023-10-17 15:02:09 +0900

    st position matters

--- app.py
+++ app.py
@@ -50,19 +50,6 @@ def get_conversation_chain(vectorstore):
         memory=memory
     )
     return conversation_chain
-
-
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
 """
 
 def main():
@@ -102,5 +89,18 @@ def main():
                     vectorstore)
 """
 
+
+
+"""def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)"""
 if __name__ == '__main__':
     main()
