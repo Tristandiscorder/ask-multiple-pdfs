13a93bfc7d20804c835d241d0e73f50cbefac1d8
Tristandiscorder <ohsama9114@gmail.com>
2023-10-17 15:04:14 +0900

    main only

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ def get_conversation_chain(vectorstore):
 def main():
     load_dotenv()
 
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
+#    st.set_page_config(page_title="Chat with multiple PDFs",
+#                       page_icon=":books:")
+#    st.write(css, unsafe_allow_html=True)
 """
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
