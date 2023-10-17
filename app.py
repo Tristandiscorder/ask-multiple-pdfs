d7f2bb0d6c177309f2d568934886744f9d5a9ebb
Tristandiscorder <ohsama9114@gmail.com>
2023-10-17 15:11:44 +0900

    sidebar check

--- app.py
+++ app.py
@@ -53,47 +53,7 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 """
-
-def main():
-    load_dotenv()
-
-    
-                       
-    st.write(css, unsafe_allow_html=True)
-"""
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
-
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-"""
-
-
-
-"""def handle_userinput(user_question):
+def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
@@ -103,6 +63,48 @@ def main():
                 "{{MSG}}", message.content), unsafe_allow_html=True)
         else:
             st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)"""
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+
+    
+                       
+    st.write(css, unsafe_allow_html=True)
+
+#    if "conversation" not in st.session_state:
+#        st.session_state.conversation = None
+#    if "chat_history" not in st.session_state:
+#        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+#        pdf_docs = st.file_uploader(
+#            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+#        if st.button("Process"):
+#            with st.spinner("Processing"):
+#                # get pdf text
+#                raw_text = get_pdf_text(pdf_docs)
+#
+#                # get the text chunks
+#                text_chunks = get_text_chunks(raw_text)
+#
+#                # create vector store
+#                vectorstore = get_vectorstore(text_chunks)
+#
+#                # create conversation chain
+#                st.session_state.conversation = get_conversation_chain(
+#                    vectorstore)
+
+
+
+
+
 if __name__ == '__main__':
     main()
